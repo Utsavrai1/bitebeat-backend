dcb311a3a6e17bfb90e445573121707a615fb47b
Utsav Rai <utsavrai890@gmail.com>
2024-06-13 21:24:54 +0530

    [Bitebeat Backend] : Updated User and Otp interfaces

--- src/interfaces/otp.ts
+++ src/interfaces/otp.ts
@@ -1,6 +1,7 @@
 import { Document } from "mongoose";
 
 export interface OtpInterface extends Document {
+  _id: string;
   email: string;
   password: string;
   name: string;
--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,6 +1,7 @@
 import { Document } from "mongoose";
 
 export interface UserInterface extends Document {
+  _id: string;
   email: string;
   password: string;
   name: string;
